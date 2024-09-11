23e3a1b63cb5ef2fe5851bb5d48f425aec9551a5
Nanda Kishore Jana <nandakishorejana780@gmail.com>
2024-09-11 13:35:06 +0530

    Create Calculate.py

---
+++ Calculate.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    print("Addition of two numbers ",a+b)
+def sub(a,b):
+    print("Substraction of two numbers: ",a-b)
+def mul(a,b):
+    print("Multiplication of two numbers: ",a*b)
+def divi(a,b):
+    print("Division of two numbers: ",a/b)
+x = int(input("Enter the 1st number: "))
+y = int(input("Enter the 2nd number: "))
+add(x,y)
+sub(x,y)
+mul(x,y)
+divi(x,y)
