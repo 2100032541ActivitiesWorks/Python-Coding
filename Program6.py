2c26efc30557e5bb5d0d68b46d954f97025a589d
Nanda Kishore Jana <nandakishorejana780@gmail.com>
2024-09-13 12:39:13 +0530

    Create Program6.py

---
+++ Program6.py
@@ -0,0 +1,12 @@
+import math 
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+n = int(input("Enter the number: "))
+print("Is it prime number:",is_prime(n))
